71906ebba1d0ca3431d2ae9641c676ac71f6aa25
Rohan Vazarkar <y2kspy@gmail.com>
2016-07-27 14:47:52 -0400

    Implement loading indicator

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -3,6 +3,7 @@ import GraphContainer from './graph'
 import SearchContainer from './SearchContainer/searchcontainer'
 import SpotlightContainer from './Spotlight/spotlightcontainer'
 import LogoutModal from './Modals/logoutmodal'
+import LoadingContainer from './loadingcontainer'
 
 export default class AppContainer extends Component {
 	constructor(){
@@ -12,6 +13,7 @@ export default class AppContainer extends Component {
 	render() {
 		return (
 			<div className="max">
+				<LoadingContainer />
 				<GraphContainer />
 				<SearchContainer />
 				<SpotlightContainer/>
