98d9f3ac8bf7df24dd049244edf00b644be74f62
Rohan Vazarkar <y2kspy@gmail.com>
2016-07-27 23:36:21 -0400

    Add right menu

---
+++ src/components/Menu/MenuContainer.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import MenuButton from './menubutton';
+
+export default class MenuContainer extends Component {
+	constructor(){
+		super()
+
+		this.state = {
+			refreshHover: false
+		}
+	}
+
+	_refreshClick(){
+
+	}
+
+	render() {
+		return (
+			<div className="menudiv">
+				<div>
+					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Refresh" glyphicon="glyphicon glyphicon-refresh" />
+				</div>
+				<div>
+					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Export Graph" glyphicon="glyphicon glyphicon-export" />
+				</div>
+				<div>
+					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Import Graph" glyphicon="glyphicon glyphicon-import" />
+				</div>
+				<div>
+					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Upload Data" glyphicon="glyphicon glyphicon-upload" />
+				</div>
+				<div>
+					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Change Layout Type" glyphicon="fa fa-line-chart" />
+				</div>
+				<div>
+					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Settings" glyphicon="fa fa-cogs" />
+				</div>
+			</div>
+		);
+	}
+}
