5e7df2177d9d3caed2e8c5532673a6c4afa08e73
Rohan Vazarkar <y2kspy@gmail.com>
2016-07-28 13:29:36 -0400

    Start adding upload

--- src/components/Menu/MenuContainer.jsx
+++ src/components/Menu/MenuContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MenuButton from './menubutton';
 const { dialog } = require('electron').remote
+var fs = require('fs')
 
 export default class MenuContainer extends Component {
 	constructor(){
@@ -25,9 +26,48 @@ export default class MenuContainer extends Component {
 	}
 
 	_importClick(){
-		emitter.emit('import',dialog.showOpenDialog({
+		var fname = dialog.showOpenDialog({
 			properties: ['openFile']
-		})[0])
+		})
+		if (typeof fname !== 'undefined'){
+			emitter.emit('import',fname[0])
+		}
+	}
+
+	_uploadClick(){
+		var data;
+
+		var filename = dialog.showOpenDialog({
+			properties: ['openFile']
+		})[0]
+
+		fs.readFile(filename, 'utf8', function(err, data){
+			var header = data.split('\n')[0]
+			var filetype;
+			if (header.includes('UserName') && header.includes('ComputerName') && header.include('Weight')){
+				filetype = 'session'
+			}else if (header.includes('AccountName') && header.includes('AccountType') && header.includes('GroupName')){
+				filetype = 'group'
+			}else if (header.includes('AccountName') && header.includes('AccountType') && header.includes('ComputerName')){
+				filetype = 'localadmin'
+			}else if (header.includes('SourceDomain') && header.includes('TargetDomain') && header.includes('TrustDirection') && header.include('TrustType') && header.includes('Transitive')){
+				filetype = 'domain'
+			}
+
+			if (typeof filetype === 'undefined'){
+				emitter.emit('showAlert', 'Unreconized CSV Type');
+				return;
+			}
+
+			Papa.parse(data,{
+				worker: true,
+				header: true,
+				dynamicTyping: true,
+				chunk: function(row, parser){
+					console.log(row)
+				}
+			})
+		})
 	}
 
 	render() {
@@ -43,7 +83,7 @@ export default class MenuContainer extends Component {
 					<MenuButton click={this._importClick.bind(this)} hoverVal="Import Graph" glyphicon="glyphicon glyphicon-import" />
 				</div>
 				<div>
-					<MenuButton click={this._refreshClick.bind(this)} hoverVal="Upload Data" glyphicon="glyphicon glyphicon-upload" />
+					<MenuButton click={this._uploadClick.bind(this)} hoverVal="Upload Data" glyphicon="glyphicon glyphicon-upload" />
 				</div>
 				<div>
 					<MenuButton click={this._changeLayoutClick.bind(this)} hoverVal="Change Layout Type" glyphicon="fa fa-line-chart" />
