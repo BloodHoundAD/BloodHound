567ee7ebf40b810e72368eaa2c542e647bb5bcb7
Rohan Vazarkar <y2kspy@gmail.com>
2016-10-11 14:53:47 -0400

    Fix typo
    Update electron-prebuilt to electron

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ response="$(curl -s --user "${GH_USER}" https://api.github.com/repos/adaptivethr
 
 iad32id="$(echo "$response" | grep -B 1 \"BloodHound-linux-ia32 | head -n1 | cut -d ":" -f 2 | cut -c 2- | sed 's/.$//')"
 x64id="$(echo "$response" | grep -B 1 \"BloodHound-linux-x64 | head -n1 | cut -d ":" -f 2 | cut -c 2- | sed 's/.$//')"
-armv71id="$(echo "$response" | grep -B 1 \"BloodHound-linux-armv71 | head -n1 | cut -d ":" -f 2 | cut -c 2- | sed 's/.$//')"
+armv7lid="$(echo "$response" | grep -B 1 \"BloodHound-linux-armv7l | head -n1 | cut -d ":" -f 2 | cut -c 2- | sed 's/.$//')"
 macid="$(echo "$response" | grep -B 1 \"BloodHound-darwin-x64 | head -n1 | cut -d ":" -f 2 | cut -c 2- | sed 's/.$//')"
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
@@ -16,13 +16,13 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 		curl -s -X DELETE --user "${GH_USER}" https://api.github.com/repos/adaptivethreat/BloodHound/releases/assets/$x64id
 	fi
 
-	if [[ ! $armv71id == "" ]]; then
-		curl -s -X DELETE --user "${GH_USER}" https://api.github.com/repos/adaptivethreat/BloodHound/releases/assets/$armv71id
+	if [[ ! $armv7lid == "" ]]; then
+		curl -s -X DELETE --user "${GH_USER}" https://api.github.com/repos/adaptivethreat/BloodHound/releases/assets/$armv7lid
 	fi
 
 	 curl -X POST -# --header 'Content-Type:application/zip' --data-binary @BloodHound-linux-ia32-$TRAVIS_COMMIT.zip --user "${GH_USER}" https://uploads.github.com/repos/adaptivethreat/BloodHound/releases/4033842/assets?name=BloodHound-linux-ia32-$TRAVIS_COMMIT.zip
 	 curl -X POST -# --header 'Content-Type:application/zip' --data-binary @BloodHound-linux-x64-$TRAVIS_COMMIT.zip --user "${GH_USER}" https://uploads.github.com/repos/adaptivethreat/BloodHound/releases/4033842/assets?name=BloodHound-linux-x64-$TRAVIS_COMMIT.zip
-	 curl -X POST -# --header 'Content-Type:application/zip' --data-binary @BloodHound-linux-armv71-$TRAVIS_COMMIT.zip --user "${GH_USER}" https://uploads.github.com/repos/adaptivethreat/BloodHound/releases/4033842/assets?name=BloodHound-linux-armv71-$TRAVIS_COMMIT.zip
+	 curl -X POST -# --header 'Content-Type:application/zip' --data-binary @BloodHound-linux-armv7l-$TRAVIS_COMMIT.zip --user "${GH_USER}" https://uploads.github.com/repos/adaptivethreat/BloodHound/releases/4033842/assets?name=BloodHound-linux-armv7l-$TRAVIS_COMMIT.zip
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
