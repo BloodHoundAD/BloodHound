940feb8bb103b5532acaa428748f995e80d7eba7
Rohan Vazarkar <y2kspy@gmail.com>
2017-01-06 03:26:34 -0500

    Convert upload to allow for huge files

--- src/components/Menu/MenuContainer.jsx
+++ src/components/Menu/MenuContainer.jsx
@@ -52,127 +52,156 @@ export default class MenuContainer extends Component {
 	}
 
 	_uploadClick(){
-		var filename = dialog.showOpenDialog({
-			properties: ['openFile']
-		})[0]
+		var selectedFile = jQuery(this.refs.fileInput)[0].files[0]
+		var filename = selectedFile.path
 		var sent = 0
-		fs.readFile(filename, 'utf8', function(err, data){
-			var count = data.split('\n').length;
-			var header = data.split('\n')[0]
-			var filetype;
-			if (header.includes('UserName') && header.includes('ComputerName') && header.includes('Weight')){
-				filetype = 'sessions'
-			}else if (header.includes('AccountName') && header.includes('AccountType') && header.includes('GroupName')){
-				filetype = 'groupmembership'
-			}else if (header.includes('AccountName') && header.includes('AccountType') && header.includes('ComputerName')){
-				filetype = 'localadmin'
-			}else if (header.includes('SourceDomain') && header.includes('TargetDomain') && header.includes('TrustDirection') && header.includes('TrustType') && header.includes('Transitive')){
-				filetype = 'domain'
-			}
 
-			if (typeof filetype === 'undefined'){
-				emitter.emit('showAlert', 'Unrecognized CSV Type');
-				return;
-			}
-
-			this.setState({
-				uploading: true,
-				progress: 0
-			})
-
-			Papa.parse(data,{
-				header: true,
-				dynamicTyping: true,
-				chunkSize: 50000,
-				skipEmptyLines: true,
-				chunk: function(rows, parser){
-					this.setState({parser: parser})
-					if (rows.data.length === 0){
-						parser.abort()
-						this.setState({progress:100})
-						setTimeout(function(){
-							this.setState({uploading: false})
-						}.bind(this), 3000)
-						emitter.emit('refreshDBData')
-						return
+		var i;
+		var count = 0;
+		var header = ""
+		var procHeader = true;
+		fs.createReadStream(filename)
+			.on('data', function(chunk) {
+				for (i=0; i < chunk.length; ++i){
+					if (procHeader){
+						header = header + String.fromCharCode(chunk[i])
 					}
-					parser.pause()
-					sent += rows.data.length
-					if (filetype === 'sessions'){
-						var query = 'UNWIND {props} AS prop MERGE (user:User {name:prop.account}) WITH user,prop MERGE (computer:Computer {name: prop.computer}) WITH user,computer,prop MERGE (computer)-[:HasSession {Weight : prop.weight}]-(user)'
-						var props = buildSessionProps(rows.data)
-						var session = driver.session()
-						session.run(query, {props: props})
-							.then(function(){
-								this.setState({progress: Math.floor((sent / count) * 100)})
-								session.close()
-								parser.resume()
-							}.bind(this))
-					}else if (filetype === 'groupmembership'){
-						var props = buildGroupMembershipProps(rows.data)
-						var userQuery = 'UNWIND {props} AS prop MERGE (user:User {name:prop.account}) WITH user,prop MERGE (group:Group {name:prop.group}) WITH user,group MERGE (user)-[:MemberOf]->(group)'
-						var computerQuery = 'UNWIND {props} AS prop MERGE (computer:Computer {name:prop.account}) WITH computer,prop MERGE (group:Group {name:prop.group}) WITH computer,group MERGE (computer)-[:MemberOf]->(group)'
-						var groupQuery = 'UNWIND {props} AS prop MERGE (group1:Group {name:prop.account}) WITH group1,prop MERGE (group2:Group {name:prop.group}) WITH group1,group2 MERGE (group1)-[:MemberOf]->(group2)'
-						var s1 = driver.session()
-						var s2 = driver.session()
-						var s3 = driver.session()
-						var p1
-						var p2
-						var p3
-						p1 = s1.run(userQuery, {props: props.users})
-						p1.then(function(){
-							s1.close()
-							p2 = s2.run(computerQuery, {props: props.computers})
-							p2.then(function(){
-								s2.close()
-								p3 = s3.run(groupQuery, {props: props.groups})
-								p3.then(function(){
-									s3.close()
+					if (chunk[i] == 10){
+						if (procHeader){
+							procHeader = false;
+						}
+						count++
+					};
+				}
+				
+			})
+			.on('end', function() {
+				count = count - 1
+				var filetype;
+				if (header.includes('UserName') && header.includes('ComputerName') && header.includes('Weight')){
+					filetype = 'sessions'
+				}else if (header.includes('AccountName') && header.includes('AccountType') && header.includes('GroupName')){
+					filetype = 'groupmembership'
+				}else if (header.includes('AccountName') && header.includes('AccountType') && header.includes('ComputerName')){
+					filetype = 'localadmin'
+				}else if (header.includes('SourceDomain') && header.includes('TargetDomain') && header.includes('TrustDirection') && header.includes('TrustType') && header.includes('Transitive')){
+					filetype = 'domain'
+				}
+
+				if (typeof filetype === 'undefined'){
+					emitter.emit('showAlert', 'Unrecognized CSV Type');
+					return;
+				}
+
+				this.setState({
+					uploading: true,
+					progress: 0
+				})
+				//I have no idea why this workaround is needed. Apparently all my sessions freeze unless I make a random query
+				setTimeout(function(){
+					var sess = driver.session()
+					sess.run('MATCH (n) RETURN (n) LIMIT 1')
+						.then(function(){
+							sess.close()
+						})
+				}, 2000)
+
+				console.time('IngestTime')
+				Papa.parse(selectedFile,{
+					header: true,
+					dynamicTyping: true,
+					skipEmptyLines: true,
+					chunkSize: 5242880,
+					//chunkSize: 500000,
+					chunk: function(rows, parser){
+						this.setState({parser: parser})
+						if (rows.data.length === 0){
+							console.timeEnd('IngestTime')
+							parser.abort()
+							this.setState({progress:100})
+							setTimeout(function(){
+								this.setState({uploading: false})
+							}.bind(this), 3000)
+							emitter.emit('refreshDBData')
+							return
+						}
+						parser.pause()
+						sent += rows.data.length
+						if (filetype === 'sessions'){
+							var query = 'UNWIND {props} AS prop MERGE (user:User {name:prop.account}) WITH user,prop MERGE (computer:Computer {name: prop.computer}) WITH user,computer,prop MERGE (computer)-[:HasSession {Weight : prop.weight}]-(user)'
+							var props = buildSessionProps(rows.data)
+							var session = driver.session()
+							session.run(query, {props: props})
+								.then(function(){
 									this.setState({progress: Math.floor((sent / count) * 100)})
+									session.close()
 									parser.resume()
 								}.bind(this))
-							}.bind(this))
-						}.bind(this))
-					}else if (filetype === 'localadmin'){
-						var props = buildLocalAdminProps(rows.data)
-						var userQuery = 'UNWIND {props} AS prop MERGE (user:User {name: prop.account}) WITH user,prop MERGE (computer:Computer {name: prop.computer}) WITH user,computer MERGE (user)-[:AdminTo]->(computer)'
-						var groupQuery = 'UNWIND {props} AS prop MERGE (group:Group {name: prop.account}) WITH group,prop MERGE (computer:Computer {name: prop.computer}) WITH group,computer MERGE (group)-[:AdminTo]->(computer)'
-						var computerQuery = 'UNWIND {props} AS prop MERGE (computer1:Computer {name: prop.account}) WITH computer1,prop MERGE (computer2:Computer {name: prop.computer}) WITH computer1,computer2 MERGE (computer1)-[:AdminTo]->(computer2)'
-
-						var s1 = driver.session()
-						var s2 = driver.session()
-						var s3 = driver.session()
-						var p1
-						var p2
-						var p3
-						p1 = s1.run(userQuery, {props: props.users})
-						p1.then(function(){
-							s1.close()
-							p2 = s2.run(computerQuery, {props: props.computers})
-							p2.then(function(){
-								s2.close()
-								p3 = s3.run(groupQuery, {props: props.groups})
-								p3.then(function(){
-									s3.close()
-									this.setState({progress: Math.floor((sent / count) * 100)})
-									parser.resume()
+						}else if (filetype === 'groupmembership'){
+							var props = buildGroupMembershipProps(rows.data)
+							var userQuery = 'UNWIND {props} AS prop MERGE (user:User {name:prop.account}) WITH user,prop MERGE (group:Group {name:prop.group}) WITH user,group MERGE (user)-[:MemberOf]->(group)'
+							var computerQuery = 'UNWIND {props} AS prop MERGE (computer:Computer {name:prop.account}) WITH computer,prop MERGE (group:Group {name:prop.group}) WITH computer,group MERGE (computer)-[:MemberOf]->(group)'
+							var groupQuery = 'UNWIND {props} AS prop MERGE (group1:Group {name:prop.account}) WITH group1,prop MERGE (group2:Group {name:prop.group}) WITH group1,group2 MERGE (group1)-[:MemberOf]->(group2)'
+							var s1 = driver.session()
+							var s2 = driver.session()
+							var s3 = driver.session()
+							var p1
+							var p2
+							var p3
+							p1 = s1.run(userQuery, {props: props.users})
+							p1.then(function(){
+								s1.close()
+								p2 = s2.run(computerQuery, {props: props.computers})
+								p2.then(function(){
+									s2.close()
+									p3 = s3.run(groupQuery, {props: props.groups})
+									p3.then(function(){
+										s3.close()
+										this.setState({progress: Math.floor((sent / count) * 100)})
+										parser.resume()
+									}.bind(this))
 								}.bind(this))
 							}.bind(this))
-						}.bind(this))
-					}else{
-						var props = buildDomainProps(rows.data)
-						var query = "UNWIND {props} AS prop MERGE (domain1:Domain {name: prop.domain1}) WITH domain1,prop MERGE (domain2:Domain {name: prop.domain2}) WITH domain1,domain2,prop MERGE (domain1)-[:TrustedBy {TrustType : prop.trusttype, Transitive: prop.transitive}]->(domain2)"
-						var session = driver.session()
-						session.run(query, {props: props})
-							.then(function(){
-								this.setState({progress: Math.floor((sent / count) * 100)})
-								session.close()
-								parser.resume()
+						}else if (filetype === 'localadmin'){
+							var props = buildLocalAdminProps(rows.data)
+							var userQuery = 'UNWIND {props} AS prop MERGE (user:User {name: prop.account}) WITH user,prop MERGE (computer:Computer {name: prop.computer}) WITH user,computer MERGE (user)-[:AdminTo]->(computer)'
+							var groupQuery = 'UNWIND {props} AS prop MERGE (group:Group {name: prop.account}) WITH group,prop MERGE (computer:Computer {name: prop.computer}) WITH group,computer MERGE (group)-[:AdminTo]->(computer)'
+							var computerQuery = 'UNWIND {props} AS prop MERGE (computer1:Computer {name: prop.account}) WITH computer1,prop MERGE (computer2:Computer {name: prop.computer}) WITH computer1,computer2 MERGE (computer1)-[:AdminTo]->(computer2)'
+
+							var s1 = driver.session()
+							var s2 = driver.session()
+							var s3 = driver.session()
+							var p1
+							var p2
+							var p3
+							p1 = s1.run(userQuery, {props: props.users})
+							p1.then(function(){
+								s1.close()
+								p2 = s2.run(computerQuery, {props: props.computers})
+								p2.then(function(){
+									s2.close()
+									p3 = s3.run(groupQuery, {props: props.groups})
+									p3.then(function(){
+										s3.close()
+										this.setState({progress: Math.floor((sent / count) * 100)})
+										parser.resume()
+									}.bind(this))
+								}.bind(this))
 							}.bind(this))
-					}
-				}.bind(this)
-			})
-		}.bind(this))
+						}else{
+							var props = buildDomainProps(rows.data)
+							var query = "UNWIND {props} AS prop MERGE (domain1:Domain {name: prop.domain1}) WITH domain1,prop MERGE (domain2:Domain {name: prop.domain2}) WITH domain1,domain2,prop MERGE (domain1)-[:TrustedBy {TrustType : prop.trusttype, Transitive: prop.transitive}]->(domain2)"
+							var session = driver.session()
+							session.run(query, {props: props})
+								.then(function(){
+									this.setState({progress: Math.floor((sent / count) * 100)})
+									session.close()
+									parser.resume()
+								}.bind(this))
+						}
+					}.bind(this)
+				})
+			}.bind(this));
 	}
 
 	_settingsClick(){
@@ -201,7 +230,7 @@ export default class MenuContainer extends Component {
 							<ProgressBarMenuButton click={this._cancelUploadClick.bind(this)} progress={this.state.progress} committed={this.state.committed}/>
 						</Then>
 						<Else>{ () =>
-							<MenuButton click={this._uploadClick.bind(this)} hoverVal="Upload Data" glyphicon="glyphicon glyphicon-upload" />		
+							<MenuButton click={function(){jQuery(this.refs.fileInput).click()}.bind(this)} hoverVal="Upload Data" glyphicon="glyphicon glyphicon-upload" />		
 						}</Else>
 					</If>		
 				</div>
@@ -211,6 +240,7 @@ export default class MenuContainer extends Component {
 				<div>
 					<MenuButton click={this._settingsClick.bind(this)} hoverVal="Settings" glyphicon="fa fa-cogs" />
 				</div>
+				<input ref="fileInput" multiple className="hide" type="file" onChange={this._uploadClick.bind(this)}/>
 			</div>
 		);
 	}
