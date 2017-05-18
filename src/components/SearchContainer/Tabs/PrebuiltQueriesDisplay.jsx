6fe6178076074251144b879590f45797d4bdf743
Rohan Vazarkar <y2kspy@gmail.com>
2017-05-18 10:15:37 -0400

    Add tooltips and refresh button to custom queries

--- src/components/SearchContainer/Tabs/PrebuiltQueriesDisplay.jsx
+++ src/components/SearchContainer/Tabs/PrebuiltQueriesDisplay.jsx
@@ -30,6 +30,23 @@ export default class PrebuiltQueriesDisplay extends Component {
         exec(this.getCommandLine() + ' ' + path.join(app.getPath('userData'),'/customqueries.json'));
     }
 
+    refreshCustom(){
+        $.ajax({
+            url: path.join(app.getPath('userData'),'/customqueries.json'),
+            type: 'GET',
+            success: function(response){
+                var x = JSON.parse(response)
+                var y = []
+
+                $.each(x.queries, function(index, el) {
+                    y.push(el)
+                });
+                
+                this.setState({custom: y})
+            }.bind(this)
+        })
+    }
+
     componentWillMount() {
         $.ajax({
             url: path.join(app.getPath('userData'),'/customqueries.json'),
@@ -75,7 +92,8 @@ export default class PrebuiltQueriesDisplay extends Component {
                 </div>
                 <h3>
                     Custom Queries
-                    <i className="glyphicon glyphicon-pencil customQueryGlyph" onClick={this.editCustom.bind(this)}></i>
+                    <i className="glyphicon glyphicon-pencil customQueryGlyph" data-toggle="tooltip" title="Edit Queries" onClick={this.editCustom.bind(this)}></i>
+                    <i className="glyphicon glyphicon-refresh customQueryGlyph" onClick={this.refreshCustom.bind(this)} style={{"padding-left": "5px"}} data-toggle="tooltip" title="Refresh Queries"></i>
                 </h3>
                 <div className="query-box">
                     <If condition={ this.state.custom.length == 0}>
