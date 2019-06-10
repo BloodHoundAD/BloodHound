369791f8dbfdfc9503e65893f222d51542dc0bb1
Rohan Vazarkar <rvazarkr@gmail.com>
2019-06-10 11:31:47 -0400

    Replace example db with new one

--- src/components/SearchContainer/TabContainer.jsx
+++ src/components/SearchContainer/TabContainer.jsx
@@ -1,172 +1,174 @@
-import React, { Component } from "react";
-import DatabaseDataDisplay from "./Tabs/DatabaseDataDisplay";
-import PrebuiltQueriesDisplay from "./Tabs/PrebuiltQueriesDisplay";
-import NoNodeData from "./Tabs/NoNodeData";
-import UserNodeData from "./Tabs/UserNodeData";
-import GroupNodeData from "./Tabs/GroupNodeData";
-import ComputerNodeData from "./Tabs/ComputerNodeData";
-import DomainNodeData from "./Tabs/DomainNodeData";
-import GpoNodeData from "./Tabs/GpoNodeData";
-import OuNodeData from "./Tabs/OuNodeData";
-import { Tabs, Tab } from "react-bootstrap";
-import { openSync, readSync, closeSync } from "fs";
-import imageType from "image-type";
+import React, { Component } from 'react';
+import DatabaseDataDisplay from './Tabs/DatabaseDataDisplay';
+import PrebuiltQueriesDisplay from './Tabs/PrebuiltQueriesDisplay';
+import NoNodeData from './Tabs/NoNodeData';
+import UserNodeData from './Tabs/UserNodeData';
+import GroupNodeData from './Tabs/GroupNodeData';
+import ComputerNodeData from './Tabs/ComputerNodeData';
+import DomainNodeData from './Tabs/DomainNodeData';
+import GpoNodeData from './Tabs/GpoNodeData';
+import OuNodeData from './Tabs/OuNodeData';
+import { Tabs, Tab } from 'react-bootstrap';
+import { openSync, readSync, closeSync } from 'fs';
+import imageType from 'image-type';
 
 export default class TabContainer extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      userVisible: false,
-      computerVisible: false,
-      groupVisible: false,
-      domainVisible: false,
-      gpoVisible: false,
-      ouVisible: false,
-      selected: 1
-    };
-  }
+        this.state = {
+            userVisible: false,
+            computerVisible: false,
+            groupVisible: false,
+            domainVisible: false,
+            gpoVisible: false,
+            ouVisible: false,
+            selected: 1,
+        };
+    }
 
-  componentDidMount() {
-    emitter.on("userNodeClicked", this._userNodeClicked.bind(this));
-    emitter.on("groupNodeClicked", this._groupNodeClicked.bind(this));
-    emitter.on("computerNodeClicked", this._computerNodeClicked.bind(this));
-    emitter.on("domainNodeClicked", this._domainNodeClicked.bind(this));
-    emitter.on("gpoNodeClicked", this._gpoNodeClicked.bind(this));
-    emitter.on("ouNodeClicked", this._ouNodeClicked.bind(this));
-    emitter.on("imageupload", this.uploadImage.bind(this));
-  }
+    componentDidMount() {
+        emitter.on('userNodeClicked', this._userNodeClicked.bind(this));
+        emitter.on('groupNodeClicked', this._groupNodeClicked.bind(this));
+        emitter.on('computerNodeClicked', this._computerNodeClicked.bind(this));
+        emitter.on('domainNodeClicked', this._domainNodeClicked.bind(this));
+        emitter.on('gpoNodeClicked', this._gpoNodeClicked.bind(this));
+        emitter.on('ouNodeClicked', this._ouNodeClicked.bind(this));
+        emitter.on('imageupload', this.uploadImage.bind(this));
+    }
 
-  uploadImage(event) {
-    let files = [];
-    $.each(event.dataTransfer.files, (_, f) => {
-      let buf = Buffer.alloc(12);
-      let file = openSync(f.path, "r");
-      readSync(file, buf, 0, 12, 0);
-      closeSync(file);
-      let type = imageType(buf);
-      if (type !== null && type.mime.includes("image")) {
-        files.push({ path: f.path, name: f.name });
-      } else {
-        emitter.emit("showAlert", `${f.name} is not an image`);
-      }
-    });
-    emitter.emit("imageUploadFinal", files);
-  }
+    uploadImage(event) {
+        let files = [];
+        $.each(event.dataTransfer.files, (_, f) => {
+            let buf = Buffer.alloc(12);
+            let file = openSync(f.path, 'r');
+            readSync(file, buf, 0, 12, 0);
+            closeSync(file);
+            let type = imageType(buf);
+            if (type !== null && type.mime.includes('image')) {
+                files.push({ path: f.path, name: f.name });
+            } else {
+                emitter.emit('showAlert', `${f.name} is not an image`);
+            }
+        });
+        emitter.emit('imageUploadFinal', files);
+    }
 
-  _userNodeClicked() {
-    this.setState({
-      userVisible: true,
-      computerVisible: false,
-      groupVisible: false,
-      domainVisible: false,
-      gpoVisible: false,
-      ouVisible: false
-    });
-    this.setState({ selected: 2 });
-  }
+    _userNodeClicked() {
+        this.setState({
+            userVisible: true,
+            computerVisible: false,
+            groupVisible: false,
+            domainVisible: false,
+            gpoVisible: false,
+            ouVisible: false,
+        });
+        this.setState({ selected: 2 });
+    }
 
-  _groupNodeClicked() {
-    this.setState({
-      userVisible: false,
-      computerVisible: false,
-      groupVisible: true,
-      domainVisible: false,
-      gpoVisible: false,
-      ouVisible: false
-    });
-    this.setState({ selected: 2 });
-  }
+    _groupNodeClicked() {
+        this.setState({
+            userVisible: false,
+            computerVisible: false,
+            groupVisible: true,
+            domainVisible: false,
+            gpoVisible: false,
+            ouVisible: false,
+        });
+        this.setState({ selected: 2 });
+    }
 
-  _computerNodeClicked() {
-    this.setState({
-      userVisible: false,
-      computerVisible: true,
-      groupVisible: false,
-      domainVisible: false,
-      gpoVisible: false,
-      ouVisible: false
-    });
-    this.setState({ selected: 2 });
-  }
+    _computerNodeClicked() {
+        this.setState({
+            userVisible: false,
+            computerVisible: true,
+            groupVisible: false,
+            domainVisible: false,
+            gpoVisible: false,
+            ouVisible: false,
+        });
+        this.setState({ selected: 2 });
+    }
 
-  _domainNodeClicked() {
-    this.setState({
-      userVisible: false,
-      computerVisible: false,
-      groupVisible: false,
-      domainVisible: true,
-      gpoVisible: false,
-      ouVisible: false
-    });
-    this.setState({ selected: 2 });
-  }
+    _domainNodeClicked() {
+        this.setState({
+            userVisible: false,
+            computerVisible: false,
+            groupVisible: false,
+            domainVisible: true,
+            gpoVisible: false,
+            ouVisible: false,
+        });
+        this.setState({ selected: 2 });
+    }
 
-  _gpoNodeClicked() {
-    this.setState({
-      userVisible: false,
-      computerVisible: false,
-      groupVisible: false,
-      domainVisible: false,
-      gpoVisible: true,
-      ouVisible: false
-    });
-    this.setState({ selected: 2 });
-  }
+    _gpoNodeClicked() {
+        this.setState({
+            userVisible: false,
+            computerVisible: false,
+            groupVisible: false,
+            domainVisible: false,
+            gpoVisible: true,
+            ouVisible: false,
+        });
+        this.setState({ selected: 2 });
+    }
 
-  _ouNodeClicked() {
-    this.setState({
-      userVisible: false,
-      computerVisible: false,
-      groupVisible: false,
-      domainVisible: false,
-      gpoVisible: false,
-      ouVisible: true
-    });
-    this.setState({ selected: 2 });
-  }
+    _ouNodeClicked() {
+        this.setState({
+            userVisible: false,
+            computerVisible: false,
+            groupVisible: false,
+            domainVisible: false,
+            gpoVisible: false,
+            ouVisible: true,
+        });
+        this.setState({ selected: 2 });
+    }
 
-  _handleSelect(index, last) {
-    this.setState({ selected: index });
-  }
+    _handleSelect(index, last) {
+        this.setState({ selected: index });
+    }
 
-  render() {
-    return (
-      <div>
-        <Tabs
-          id="tab-style"
-          bsStyle="pills"
-          activeKey={this.state.selected}
-          onSelect={this._handleSelect.bind(this)}
-        >
-          <Tab eventKey={1} title="Database Info">
-            <DatabaseDataDisplay />
-          </Tab>
+    render() {
+        return (
+            <div>
+                <Tabs
+                    id='tab-style'
+                    bsStyle='pills'
+                    activeKey={this.state.selected}
+                    onSelect={this._handleSelect.bind(this)}
+                >
+                    <Tab eventKey={1} title='Database Info'>
+                        <DatabaseDataDisplay />
+                    </Tab>
 
-          <Tab eventKey={2} title="Node Info">
-            <NoNodeData
-              visible={
-                !this.state.userVisible &&
-                !this.state.computerVisible &&
-                !this.state.groupVisible &&
-                !this.state.domainVisible &&
-                !this.state.gpoVisible &&
-                !this.state.ouVisible
-              }
-            />
-            <UserNodeData visible={this.state.userVisible} />
-            <GroupNodeData visible={this.state.groupVisible} />
-            <ComputerNodeData visible={this.state.computerVisible} />
-            <DomainNodeData visible={this.state.domainVisible} />
-            <GpoNodeData visible={this.state.gpoVisible} />
-            <OuNodeData visible={this.state.ouVisible} />
-          </Tab>
+                    <Tab eventKey={2} title='Node Info'>
+                        <NoNodeData
+                            visible={
+                                !this.state.userVisible &&
+                                !this.state.computerVisible &&
+                                !this.state.groupVisible &&
+                                !this.state.domainVisible &&
+                                !this.state.gpoVisible &&
+                                !this.state.ouVisible
+                            }
+                        />
+                        <UserNodeData visible={this.state.userVisible} />
+                        <GroupNodeData visible={this.state.groupVisible} />
+                        <ComputerNodeData
+                            visible={this.state.computerVisible}
+                        />
+                        <DomainNodeData visible={this.state.domainVisible} />
+                        <GpoNodeData visible={this.state.gpoVisible} />
+                        <OuNodeData visible={this.state.ouVisible} />
+                    </Tab>
 
-          <Tab eventKey={3} title="Queries">
-            <PrebuiltQueriesDisplay />
-          </Tab>
-        </Tabs>
-      </div>
-    );
-  }
+                    <Tab eventKey={3} title='Queries'>
+                        <PrebuiltQueriesDisplay />
+                    </Tab>
+                </Tabs>
+            </div>
+        );
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import "babel-polyfill"; // generators
-import React from "react";
-import ReactDOM from "react-dom";
+import 'babel-polyfill'; // generators
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import AppContainer from "./AppContainer";
-import Login from "./components/Float/Login";
+import AppContainer from './AppContainer';
+import Login from './components/Float/Login';
 
-import { remote, shell } from "electron";
+import { remote, shell } from 'electron';
 const { app } = remote;
-import { join } from "path";
-import { stat, writeFile, existsSync, mkdirSync } from "fs";
+import { join } from 'path';
+import { stat, writeFile, existsSync, mkdirSync } from 'fs';
 
-import ConfigStore from "electron-store";
+import ConfigStore from 'electron-store';
 global.conf = new ConfigStore();
 global.imageconf = new ConfigStore({
-    name: "images"
-})
-import { EventEmitter2 as e } from "eventemitter2";
+    name: 'images',
+});
+import { EventEmitter2 as e } from 'eventemitter2';
 global.emitter = new e({});
 emitter.setMaxListeners(0);
 global.renderEmit = new e({});
-global.neo4j = require("neo4j-driver").v1;
-global.Mustache = require("mustache");
+global.neo4j = require('neo4j-driver').v1;
+global.Mustache = require('mustache');
 
 //open links externally by default
 $(document).on('click', 'a[href^="http"]', function(event) {
@@ -32,7 +32,7 @@ String.prototype.format = function() {
     var i = 0,
         args = arguments;
     return this.replace(/{}/g, function() {
-        return typeof args[i] !== "undefined" ? args[i++] : "";
+        return typeof args[i] !== 'undefined' ? args[i++] : '';
     });
 };
 
@@ -61,8 +61,8 @@ Array.prototype.chunk = function() {
     let temp = [];
     let chunkSize = 10000;
 
-    for (i = 0; i < len; i+= chunkSize){
-        temp.push(this.slice(i, i+chunkSize));
+    for (i = 0; i < len; i += chunkSize) {
+        temp.push(this.slice(i, i + chunkSize));
     }
 
     return temp;
@@ -76,15 +76,15 @@ if (!Array.prototype.last) {
 
 sigma.renderers.def = sigma.renderers.canvas;
 
-sigma.classes.graph.addMethod("outboundNodes", function(id) {
+sigma.classes.graph.addMethod('outboundNodes', function(id) {
     return this.outNeighborsIndex.get(id).keyList();
 });
 
-sigma.classes.graph.addMethod("inboundNodes", function(id) {
+sigma.classes.graph.addMethod('inboundNodes', function(id) {
     return this.inNeighborsIndex.get(id).keyList();
 });
 
-sigma.classes.graph.addMethod("outNeighbors", function(id) {
+sigma.classes.graph.addMethod('outNeighbors', function(id) {
     return this.outNeighborsIndex.get(id).keyList();
 });
 
@@ -101,214 +101,214 @@ global.appStore = {
         iconScheme: {
             User: {
                 font: "'Font Awesome 5 Free'",
-                content: "\uf007",
+                content: '\uf007',
                 scale: 1.5,
-                color: "#17E625"
+                color: '#17E625',
             },
             Computer: {
                 font: "'Font Awesome 5 Free'",
-                content: "\uF108",
+                content: '\uF108',
                 scale: 1.2,
-                color: "#E67873"
+                color: '#E67873',
             },
             Group: {
                 font: "'Font Awesome 5 Free'",
-                content: "\uF0C0",
+                content: '\uF0C0',
                 scale: 1.5,
-                color: "#DBE617"
+                color: '#DBE617',
             },
             Domain: {
                 font: "'Font Awesome 5 Free'",
-                content: "\uF0AC",
+                content: '\uF0AC',
                 scale: 1.5,
-                color: "#17E6B9"
+                color: '#17E6B9',
             },
             OU: {
                 font: "'Font Awesome 5 Free'",
-                content: "\uF0E8",
+                content: '\uF0E8',
                 scale: 1.25,
-                color: "#FFAA00"
+                color: '#FFAA00',
             },
             GPO: {
                 font: "'Font Awesome 5 Free'",
-                content: "\uF03A",
+                content: '\uF03A',
                 scale: 1.25,
-                color: "#7F72FD"
-            }
+                color: '#7F72FD',
+            },
         },
         edgeScheme: {
-            AdminTo: "tapered",
-            MemberOf: "tapered",
-            HasSession: "tapered",
-            AllExtendedRights: "tapered",
-            ForceChangePassword: "tapered",
-            GenericAll: "tapered",
-            GenericWrite: "tapered",
-            WriteDacl: "tapered",
-            WriteOwner: "tapered",
-            AddMember: "tapered",
-            TrustedBy: "curvedArrow",
-            DCSync: "tapered",
-            Contains: "tapered",
-            GpLink: "tapered",
-            Owns: "tapered",
-            CanRDP: "tapered",
-            ExecuteDCOM: "tapered",
-            ReadLAPSPassword: "tapered",
-            AllowedToDelegate: "tapered",
-            AddAllowedToAct: "tapered",
-            AllowedToAct: "tapered",
-            GetChanges: "tapered",
-            GetChangeAll: "tapered",
-            SQLAdmin: "tapered"
-        }
+            AdminTo: 'tapered',
+            MemberOf: 'tapered',
+            HasSession: 'tapered',
+            AllExtendedRights: 'tapered',
+            ForceChangePassword: 'tapered',
+            GenericAll: 'tapered',
+            GenericWrite: 'tapered',
+            WriteDacl: 'tapered',
+            WriteOwner: 'tapered',
+            AddMember: 'tapered',
+            TrustedBy: 'curvedArrow',
+            DCSync: 'tapered',
+            Contains: 'tapered',
+            GpLink: 'tapered',
+            Owns: 'tapered',
+            CanRDP: 'tapered',
+            ExecuteDCOM: 'tapered',
+            ReadLAPSPassword: 'tapered',
+            AllowedToDelegate: 'tapered',
+            AddAllowedToAct: 'tapered',
+            AllowedToAct: 'tapered',
+            GetChanges: 'tapered',
+            GetChangeAll: 'tapered',
+            SQLAdmin: 'tapered',
+        },
     },
     lowResPalette: {
         colorScheme: {
-            User: "#17E625",
-            Computer: "#E67873",
-            Group: "#DBE617",
-            Domain: "#17E6B9",
-            OU: "#FFAA00",
-            GPO: "#7F72FD"
+            User: '#17E625',
+            Computer: '#E67873',
+            Group: '#DBE617',
+            Domain: '#17E6B9',
+            OU: '#FFAA00',
+            GPO: '#7F72FD',
         },
         edgeScheme: {
-            AdminTo: "line",
-            MemberOf: "line",
-            HasSession: "line",
-            AllExtendedRights: "line",
-            ForceChangePassword: "line",
-            GenericAll: "line",
-            GenericWrite: "line",
-            WriteDacl: "line",
-            WriteOwner: "line",
-            AddMember: "line",
-            TrustedBy: "curvedArrow",
-            DCSync: "line",
-            Contains: "line",
-            GpLink: "line",
-            Owns: "line",
-            CanRDP: "line",
-            ExecuteDCOM: "line",
-            ReadLAPSPassword: "line",
-            AllowedToDelegate: "line",
-            AddAllowedToAct: "line",
-            AllowedToAct: "line",
-            GetChanges: "line",
-            GetChangeAll: "line",
-            SQLAdmin: "line"
-        }
+            AdminTo: 'line',
+            MemberOf: 'line',
+            HasSession: 'line',
+            AllExtendedRights: 'line',
+            ForceChangePassword: 'line',
+            GenericAll: 'line',
+            GenericWrite: 'line',
+            WriteDacl: 'line',
+            WriteOwner: 'line',
+            AddMember: 'line',
+            TrustedBy: 'curvedArrow',
+            DCSync: 'line',
+            Contains: 'line',
+            GpLink: 'line',
+            Owns: 'line',
+            CanRDP: 'line',
+            ExecuteDCOM: 'line',
+            ReadLAPSPassword: 'line',
+            AllowedToDelegate: 'line',
+            AddAllowedToAct: 'line',
+            AllowedToAct: 'line',
+            GetChanges: 'line',
+            GetChangeAll: 'line',
+            SQLAdmin: 'line',
+        },
     },
     highResStyle: {
         nodes: {
             label: {
-                by: "label"
+                by: 'label',
             },
             size: {
-                by: "degree",
+                by: 'degree',
                 bins: 5,
                 min: 10,
-                max: 20
+                max: 20,
             },
             icon: {
-                by: "type",
-                scheme: "iconScheme"
-            }
+                by: 'type',
+                scheme: 'iconScheme',
+            },
         },
         edges: {
             type: {
-                by: "type",
-                scheme: "edgeScheme"
+                by: 'type',
+                scheme: 'edgeScheme',
             },
             size: {
-                by: "degree",
+                by: 'degree',
                 bins: 1,
                 min: 4,
-                max: 4
-            }
-        }
+                max: 4,
+            },
+        },
     },
     lowResStyle: {
         nodes: {
             label: {
-                by: "label"
+                by: 'label',
             },
             size: {
-                by: "degree",
+                by: 'degree',
                 bins: 10,
                 min: 10,
-                max: 20
+                max: 20,
             },
             color: {
-                by: "type",
-                scheme: "colorScheme"
-            }
+                by: 'type',
+                scheme: 'colorScheme',
+            },
         },
         edges: {
             type: {
-                by: "type",
-                scheme: "edgeScheme"
+                by: 'type',
+                scheme: 'edgeScheme',
             },
             size: {
-                by: "degree",
+                by: 'degree',
                 bins: 1,
                 min: 4,
-                max: 4
-            }
-        }
-    }
+                max: 4,
+            },
+        },
+    },
 };
 
-if (typeof conf.get("performance") === "undefined") {
-    conf.set("performance", {
+if (typeof conf.get('performance') === 'undefined') {
+    conf.set('performance', {
         edge: 5,
         lowGraphics: false,
         nodeLabels: 0,
         edgeLabels: 0,
-        darkMode: false
+        darkMode: false,
     });
 }
 
-if (typeof conf.get("edgeincluded") === "undefined"){
-    conf.set("edgeincluded", {
-        MemberOf : true,
+if (typeof conf.get('edgeincluded') === 'undefined') {
+    conf.set('edgeincluded', {
+        MemberOf: true,
         HasSession: true,
         AdminTo: true,
         AllExtendedRights: true,
         AddMember: true,
         ForceChangePassword: true,
-        GenericAll:true,
-        GenericWrite:true,
+        GenericAll: true,
+        GenericWrite: true,
         Owns: true,
         WriteDacl: true,
         WriteOwner: true,
-        CanRDP:true,
-        ExecuteDCOM:true,
+        CanRDP: true,
+        ExecuteDCOM: true,
         AllowedToDelegate: true,
         ReadLAPSPassword: true,
         Contains: true,
         GpLink: true,
         AddAllowedToAct: true,
         AllowedToAct: true,
-        SQLAdmin: true
+        SQLAdmin: true,
     });
 }
 
-appStore.edgeincluded = conf.get("edgeincluded");
+appStore.edgeincluded = conf.get('edgeincluded');
 //Code to add new edges to filter
-if (!appStore.edgeincluded.hasOwnProperty("AddAllowedToAct")){
+if (!appStore.edgeincluded.hasOwnProperty('AddAllowedToAct')) {
     appStore.edgeincluded.AddAllowedToAct = true;
-    conf.set("edgeincluded", appStore.edgeincluded);
+    conf.set('edgeincluded', appStore.edgeincluded);
 }
 
-if (!appStore.edgeincluded.hasOwnProperty("AllowedToAct")) {
+if (!appStore.edgeincluded.hasOwnProperty('AllowedToAct')) {
     appStore.edgeincluded.AllowedToAct = true;
-    conf.set("edgeincluded", appStore.edgeincluded);
+    conf.set('edgeincluded', appStore.edgeincluded);
 }
 
-if (!appStore.edgeincluded.hasOwnProperty("SQLAdmin")) {
+if (!appStore.edgeincluded.hasOwnProperty('SQLAdmin')) {
     appStore.edgeincluded.SQLAdmin = true;
-    conf.set("edgeincluded", appStore.edgeincluded);
+    conf.set('edgeincluded', appStore.edgeincluded);
 }
 
 // if (!appStore.edgeincluded.hasOwnProperty("ReadLAPSPassword")) {
@@ -316,50 +316,48 @@ if (!appStore.edgeincluded.hasOwnProperty("SQLAdmin")) {
 //     conf.set("edgeincluded", appStore.edgeincluded)
 // }
 
-appStore.performance = conf.get("performance");
+appStore.performance = conf.get('performance');
 
-if (typeof appStore.performance.edgeLabels === "undefined") {
+if (typeof appStore.performance.edgeLabels === 'undefined') {
     appStore.performance.edgeLabels = 0;
-    conf.set("performance", appStore.performance);
+    conf.set('performance', appStore.performance);
 }
 
-if (typeof appStore.performance.darkMode === "undefined") {
+if (typeof appStore.performance.darkMode === 'undefined') {
     appStore.performance.darkMode = false;
-    conf.set("performance", appStore.performance);
+    conf.set('performance', appStore.performance);
 }
 
-var custompath = join(app.getPath("userData"), "customqueries.json");
+var custompath = join(app.getPath('userData'), 'customqueries.json');
 
 stat(custompath, function(err, stats) {
     if (err) {
-        writeFile(custompath, "{}");
+        writeFile(custompath, '{}');
     }
 });
 
-let imagepath = join(app.getPath("userData"), "images");
-if (!existsSync(imagepath)){
-    mkdirSync(imagepath)
+let imagepath = join(app.getPath('userData'), 'images');
+if (!existsSync(imagepath)) {
+    mkdirSync(imagepath);
 }
 
-global.closeTooltip = function(){
-    if (appStore.currentTooltip !== null){
+global.closeTooltip = function() {
+    if (appStore.currentTooltip !== null) {
         appStore.currentTooltip.close();
         appStore.currentTooltip = null;
     }
-}
+};
 
-renderEmit.on("login", function() {
+renderEmit.on('login', function() {
     emitter.removeAllListeners();
-    ReactDOM.unmountComponentAtNode(document.getElementById("root"));
-    ReactDOM.render(<AppContainer />, document.getElementById("root"));
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+    ReactDOM.render(<AppContainer />, document.getElementById('root'));
 });
 
-renderEmit.on("logout", function() {
+renderEmit.on('logout', function() {
     emitter.removeAllListeners();
-    ReactDOM.unmountComponentAtNode(document.getElementById("root"));
-    ReactDOM.render(<Login />, document.getElementById("root"));
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+    ReactDOM.render(<Login />, document.getElementById('root'));
 });
 
-ReactDOM.render(<Login />, document.getElementById("root"));
-
-
+ReactDOM.render(<Login />, document.getElementById('root'));
