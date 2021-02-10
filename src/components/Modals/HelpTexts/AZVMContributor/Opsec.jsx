ab323ec41420ba5a538c1834e8d2a14785ab9dd4
Andy Robbins <robbins.andy@gmail.com>
2021-02-10 12:20:41 -0600

    Update Opsec.jsx

--- src/components/Modals/HelpTexts/AZVMContributor/Opsec.jsx
+++ src/components/Modals/HelpTexts/AZVMContributor/Opsec.jsx
@@ -1,5 +1,5 @@
 const Opsec = () => {
-    let text = ``;
+    let text = `Because you'll be running a command as the SYSTEM user on the Virtual Machine, the same opsec considerations for running malicious commands on any system should be taken into account: command line logging, PowerShell script block logging, EDR, etc.`;
     return { __html: text };
 };
 
