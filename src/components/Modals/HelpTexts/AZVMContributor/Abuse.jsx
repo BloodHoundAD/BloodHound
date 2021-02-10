e4255d79eeadb5748b026ba7af9b0b76d8ed7c0f
Andy Robbins <robbins.andy@gmail.com>
2021-02-10 12:18:47 -0600

    Update Abuse.jsx

--- src/components/Modals/HelpTexts/AZVMContributor/Abuse.jsx
+++ src/components/Modals/HelpTexts/AZVMContributor/Abuse.jsx
@@ -1,6 +1,14 @@
 import { typeFormat } from '../Formatter';
 const Abuse = (sourceName, sourceType, targetName, targetType) => {
-    let text = ``;
+    let text = `The Virtual Machine Contributor role allows you to run SYSTEM commands on the VM
+    <strong>Resource Group</strong>: NOT abusable, and not collected by AzureHound
+
+    Via PowerZure:
+    <a href="http://Invoke-AzureRunCommand">Invoke-AzureRunCommand</a>
+    <a href="http://Invoke-AzureRunMSBuild">Invoke-AzureRunMSBuild</a>
+    <a href="https://powerzure.readthedocs.io/en/latest/Functions/operational.html#invoke-azurerunprogram">Invoke-AzureRunProgram</a>
+    `;
     return { __html: text };
 };
+
 export default Abuse;
