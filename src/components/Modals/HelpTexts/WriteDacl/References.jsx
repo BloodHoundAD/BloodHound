ef733213bf72ebcbbc22361d8b97afcfb0df5bd3
Andy Robbins <robbins.andy@gmail.com>
2022-01-19 16:43:29 -0800

    Add reference to work by @n00py

--- src/components/Modals/HelpTexts/WriteDacl/References.jsx
+++ src/components/Modals/HelpTexts/WriteDacl/References.jsx
@@ -3,6 +3,7 @@ const References = () => {
             <a href="https://www.youtube.com/watch?v=z8thoG7gPd0">https://www.youtube.com/watch?v=z8thoG7gPd0</a>
             <a href="https://eladshamir.com/2019/01/28/Wagging-the-Dog.html">https://eladshamir.com/2019/01/28/Wagging-the-Dog.html</a>
             <a href="https://github.com/GhostPack/Rubeus#s4u">https://github.com/GhostPack/Rubeus#s4u</a>
+            <a href="https://github.com/n00py/DCSync">https://github.com/n00py/DCSync</a>
             <a href="https://gist.github.com/HarmJ0y/224dbfef83febdaf885a8451e40d52ff">https://gist.github.com/HarmJ0y/224dbfef83febdaf885a8451e40d52ff</a>
             <a href="http://www.harmj0y.net/blog/redteaming/another-word-on-delegation/">http://www.harmj0y.net/blog/redteaming/another-word-on-delegation/</a>
             <a href="https://github.com/PowerShellMafia/PowerSploit/blob/dev/Recon/PowerView.ps1">https://github.com/PowerShellMafia/PowerSploit/blob/dev/Recon/PowerView.ps1</a>
