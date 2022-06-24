bfb9c4df159252316a7dc768e7dc282eee186d51
Rohan Vazarkar <rvazarkr@gmail.com>
2022-06-24 11:43:15 -0400

    fix: remove old post-processing query

--- src/components/Menu/MenuContainer.jsx
+++ src/components/Menu/MenuContainer.jsx
@@ -383,13 +383,6 @@ const MenuContainer = () => {
         setNeedsPostProcess(false)
         let session = driver.session();
 
-        const highValueSids = ["-544", "-500", "-512", "-516", "-518", "-519", "1-5-9", "-526", "-527", "-551"]
-        const highValueStatement = "UNWIND $sids AS sid MATCH (n:Base) WHERE n.objectid ENDS WITH sid SET n.highvalue=true"
-
-        await session.run(highValueStatement, {sids: highValueSids}).catch((err) => {
-            console.log(err);
-        });
-
         const baseOwnedStatement = "MATCH (n) WHERE n:User or n:Computer AND NOT EXISTS(n.owned) SET n.owned = false"
         await session.run(baseOwnedStatement, null).catch((err) => {
             console.log(err);
