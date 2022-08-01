0c35068265c959699907dd410ed03e601c44ce43
Andy Robbins <robbins.andy@gmail.com>
2022-08-01 12:43:12 -0700

    Update AZHasRole opsec modal text

--- src/components/Modals/HelpTexts/AZHasRole/Opsec.jsx
+++ src/components/Modals/HelpTexts/AZHasRole/Opsec.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Opsec = () => {
-    return <></>;
+    return (
+        <p>
+            The opsec considerations for a particuar action authorized
+            by a principal's active AzureAD role assignment will wholly
+            depend on what the action taken is. This edge does not capture
+            all abusable possibilities.
+        </p>
+    );
 };
 
 export default Opsec;
