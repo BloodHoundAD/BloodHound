20a63b6a9d91588381fa70fb66a13250d5fa97fa
Andy Robbins <robbins.andy@gmail.com>
2022-08-01 12:49:59 -0700

    Update AZMemberOf references modal text

--- src/components/Modals/HelpTexts/AZMemberOf/References.jsx
+++ src/components/Modals/HelpTexts/AZMemberOf/References.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const References = () => {
-    return <></>;
+    return (
+        <>
+            <a href='https://docs.microsoft.com/en-us/azure/active-directory/roles/groups-create-eligible'>
+                Create a role-assignable group in Azure Active Directory
+            </a>
+        </>
+    );
 };
 
 export default References;
