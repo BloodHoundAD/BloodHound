fdb59b005fa004ce5c5799300324b48a7acbc210
Andy Robbins <robbins.andy@gmail.com>
2022-08-01 13:12:08 -0700

    Update AZVMAdminLogin abuse modal text

--- src/components/Modals/HelpTexts/AZVMAdminLogin/Abuse.jsx
+++ src/components/Modals/HelpTexts/AZVMAdminLogin/Abuse.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Abuse = () => {
-    return <></>;
+    return (
+        <>
+            <p>
+                Connect to the VM via RDP and you will be granted local admin rights
+                on the VM.
+            </p>
+        </>
+    );
 };
 
 export default Abuse;
