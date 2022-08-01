83e44580e35991e6fa69a986ca2da220c8b419f0
Andy Robbins <robbins.andy@gmail.com>
2022-08-01 12:48:03 -0700

    Update AZMemberOf abuse modal text

--- src/components/Modals/HelpTexts/AZMemberOf/Abuse.jsx
+++ src/components/Modals/HelpTexts/AZMemberOf/Abuse.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Abuse = () => {
-    return <></>;
+    return (
+        <p>
+            No abuse is necessary. This edge simply indicates that a principal
+            belongs to a security group.
+        </p>
+    );
 };
 
 export default Abuse;
