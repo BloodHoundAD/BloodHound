7af20be3318ce72156de0dd250201ded37667f71
Andy Robbins <robbins.andy@gmail.com>
2022-08-01 12:41:37 -0700

    Update AZHasRole general modal text

--- src/components/Modals/HelpTexts/AZHasRole/General.jsx
+++ src/components/Modals/HelpTexts/AZHasRole/General.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const General = () => {
-    return <></>;
+    return (
+        <p>
+            This edge indicates that a principal has been
+            granted a particular AzureAD admin role.
+        </p>
+    );
 };
 
 export default General;
