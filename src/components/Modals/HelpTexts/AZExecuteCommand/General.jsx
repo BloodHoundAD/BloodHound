fb61577c71a1c3742726e189813920b2e42718ad
Andy Robbins <robbins.andy@gmail.com>
2022-08-01 12:25:53 -0700

    Update AZExecuteCommand general text

--- src/components/Modals/HelpTexts/AZExecuteCommand/General.jsx
+++ src/components/Modals/HelpTexts/AZExecuteCommand/General.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const General = () => {
-    return <></>;
+    return (
+        <p>
+            Principals with the Intune Administrators role are able to
+            execute arbitrary PowerShell scripts on devices that are joined
+            to the Azure Active Directory tenant.
+        </p>
+    );
 };
 
 export default General;
